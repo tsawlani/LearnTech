72986511e6cac1ecd3759dded7446476547e7a2a
tarun sawlani <tarun.sawlani@gmail.com>
2018-07-07 20:56:57 +0100

    This is a test project to check the git hub configuration

---
+++ TestProject/src/main/java/com/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("this is my first test application from my first new Macbook Pro");
+	}
+
+}
